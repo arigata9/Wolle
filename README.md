ef046e4387295813dbdd8b7b0ccef4e2fad67381
Markus N <markus@markuss-ipro.speedport.ip>
2019-12-29 18:59:03 +0100

    Adding a basic login routine

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Wolle
 WoL web frontend . Access Wolle from web and start configurated machine. Just what WoL does.
+
+## Before you start
+Make sure that you edit config.json before starting this app.
--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const wol = require('wol');
 const express = require('express');
 const config = require('./config.json');
+const basicAuth = require('express-basic-auth');
 var app = express();
+
+app.use(basicAuth({
+    users: config.users,
+    unauthorizedResponse: getUnauthorizedResponse,
+    challenge: true
+}));
 
 app.use(express.static('static'));
 app.get('/', function(req, res) {
@@ -14,17 +21,27 @@ app.get('/get_wol', function(req, res) {
     wol.wake(mac, function(err, result) {
         if(result === true) {
             console.log('SUCCESS :: Wol was sent!');
-            res.send('Wolle versendet, Sir!');
+            res.sendFile(__dirname + "/static/" + "wolsent.html");
         } else {
             console.log('ERROR :: could not send Wol! Please check MAC in config.json');
-            res.send('Konnte Wolle nicht versenden, Sir! Ceck config');
+            res.send('Konnte Wolle nicht versenden, Sir! Check config');
         }
     });
 });
 
-var server = app.listen(1337, function() {
+app.get('/logout', function(req, res) {
+    res.status(401).end();
+})
+
+function getUnauthorizedResponse(req) {
+    return req.auth
+        ? ('<h3>Provided credentials for ' + req.auth.user + ' rejected!</h3>')
+        : '<h3>Unauthorized (401)</h3>'
+}
+
+var server = app.listen(1337, config.serverAddr, function() {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log(`App is online and listening at Port ${port}`);
+    console.log(`App is online and listening at http://${host}:${port}`);
 });
